add746646151c8de9c582da910e83a4d93cd429b
Brian Moss <bmoss@redhat.com>
2013-11-20 10:05:57 +1000

    Catch website error exception.

--- dailyzen.py
+++ dailyzen.py
@@ -5,10 +5,6 @@ import requests
 from bs4 import BeautifulSoup, Comment
 from sys import exit
 
-def checksite(r):
-	if r.status_code == 200: return True
-	else: return False
-
 def quote(r):
 	soup = BeautifulSoup(r.text, from_encoding='utf-8')
 	comments = soup.findAll(text=lambda text:isinstance(text, Comment))
@@ -22,14 +18,13 @@ def quote(r):
 	return
 
 def main():
-	r = requests.get('http://www.dailyzen.com/')
-	if checksite(r):
-		quote(r)
-	else:
-		print 'Website error: ' + r.status_code
-		response = raw_input('Retry? (y/n): ')
+	try:
+		r = requests.get('http://www.dailyzen.com/')
+	except:
+		response = raw_input('\nWebsite error\nRetry? (y/n): ')
 		if response == 'y': main()
-		else: sys.exit(0)
+		else: exit(0)
+	quote(r)
 
 if __name__ == '__main__':
 	main()
