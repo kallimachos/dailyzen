6c08af011bc3fa3c19bdb6a064461788b666f380
kallimachos <kallimachos@gmail.com>
2015-11-13 12:17:27 +1000

    Add tox and travis

--- dailyzen.py
+++ dailyzen.py
@@ -1,31 +1,37 @@
-#This app grabs the text from dailyzen and prints it to the command line.
-#It uses the beautifulsoup and requests libraries to achieve this.
+# This app grabs the text from dailyzen and prints it to the command line.
+# It uses the beautifulsoup and requests libraries to achieve this.
 
 import requests
 from bs4 import BeautifulSoup, Comment
 from sys import exit
 
+
 def quote(r):
-	soup = BeautifulSoup(r.text, from_encoding='utf-8')
-	comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-	[comment.extract() for comment in comments]
-	soup = soup.p
-	print('')
-	for string in soup.stripped_strings:
-		string = string.replace(u'\x96','-')
-		string = string.replace(u'\x97','-')
-		if string[0] == '-': print('\n' + string + '\n')
-		else: print(string)
-	return
+    soup = BeautifulSoup(r.text, from_encoding='utf-8')
+    comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+    [comment.extract() for comment in comments]
+    soup = soup.p
+    print('')
+    for string in soup.stripped_strings:
+        string = string.replace(u'\x96', '-')
+        string = string.replace(u'\x97', '-')
+        if string[0] == '-':
+            print('\n' + string + '\n')
+        else:
+            print(string)
+    return
+
 
 def main():
-	try:
-		r = requests.get('http://www.dailyzen.com/')
-	except:
-		response = raw_input('\nWebsite error\nRetry? (y/n): ')
-		if response == 'y': main()
-		else: exit(0)
-	quote(r)
+    try:
+        r = requests.get('http://www.dailyzen.com/')
+    except:
+        response = input('\nWebsite error\nRetry? (y/n): ')
+        if response == 'y':
+            main()
+        else:
+            exit(0)
+    quote(r)
 
 if __name__ == '__main__':
-	main()
+    main()
---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/bin/python
+
+from distutils.core import setup
+
+setup(name='dailyzen',
+      version='1.0',
+      py_modules=['dailyzen'],
+      )
